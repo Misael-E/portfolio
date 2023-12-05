5e781fb60df0c6a0ea165f8b5885b9bf3b17d09a
Misael-E <misael.esperanzate@hotmail.com>
2023-12-05 16:25:09 -0700

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ const Home = async (props: Props) => {
 		await fetchAllData();
 	return (
 		<div
-			className="bg-[rgb(36,36,36)] text-white h-full snap-y snap-mandatory 
+			className="bg-[rgb(36,36,36)] text-white max-h-full h-screen snap-y snap-mandatory 
 		overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 
 		scrollbar-thumb-[#AD343E]/80"
 		>
