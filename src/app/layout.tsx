67a63c25670109ef16c66c7fd0801add7ffbc311
Misael-E <misael.esperanzate@hotmail.com>
2023-12-05 16:00:41 -0700

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,8 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
 	width: "device-width",
-	initialScale: 0.95,
-	maximumScale: 0.95,
+	initialScale: 1,
+	maximumScale: 1,
 };
 
 export default function RootLayout({
