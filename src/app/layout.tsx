e93cd7eafc87b1415f902e383b2f6775592e36f8
Misael-E <misael.esperanzate@hotmail.com>
2023-12-05 17:00:26 -0700

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,8 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
 	width: "device-width",
-	initialScale: 1,
-	maximumScale: 1,
+	initialScale: 0.85,
+	maximumScale: 0.85,
 };
 
 export default function RootLayout({
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -27,7 +27,7 @@ const About = ({ pageInfo }: Props) => {
 				whileInView={{ x: 0 }}
 				viewport={{ once: true }}
 				src={urlFor(pageInfo?.profilePic).url()}
-				className="-mb-20 md:mb-0 mt-10 md:ml-10 flex-shrink-0 w-32 h-32 rounded-full object-cover 
+				className="-mb-24 md:mb-0 mt-10 md:ml-10 flex-shrink-0 w-40 h-40 rounded-full object-cover 
 				md:rounded-lg md:w-64 md:h-96 xl:w-[31.5rem] xl:h-[37.5rem]"
 			/>
 
